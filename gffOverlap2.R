993898727b7038813727b4710712ba2603f8b3d5
zhushijia <sjzhu.bioinfo@gmail.com>
2018-03-02 21:59:08 -0500

    venn diagram

--- gffOverlap2.R
+++ gffOverlap2.R
@@ -3,6 +3,7 @@ library(STAR2bSMRT,lib.loc="/hpc/users/zhus02/schzrnas/sjzhu/Project/NRXN/code/S
 folder="/hpc/users/zhus02/schzrnas/sjzhu/Project/NRXN/result/STAR2bSMRT/pipeline_nonAdjustNCjunc/"
 setwd(folder)
 samples= dir()
+samples = samples[samples!="combinedSamples"]
 
 gffs = lapply( samples , function(sample)
 {
@@ -15,6 +16,16 @@ names(gffs) = samples
 exp = lapply( gffs , function(x) {
   sapply( strsplit(names(x),'_'), function(p) as.integer(gsub("exp|;","",p[4])) )
 }   )
+
+
+hiPSC = unionGff(gffs[[1]],gffs[[2]])
+humanBrain = unionGff(unionGff(gffs[[6]],gffs[[7]]),gffs[[8]])
+fetal = gffs[[9]]
+patient = unionGff(gffs[[3]],gffs[[4]])
+mergeGffs = list( hiPSC=hiPSC , adult=humanBrain , fetal=fetal , patient=patient )
+
+
+
 
 
 setwd("/sc/orga/projects/schzrnas/sjzhu/Project/NRXN/result/STAR2bSMRT/result/pipeline_nonAdjustNCjunc")
@@ -47,21 +58,23 @@ dev.off()
 
 
 
-library(VennDiagram)
-range = c(1:5)
-tlist = lapply( gffs[range] , function(y) lapply( y , function(x) paste(paste(x[,1],x[,2])))   )
-tall = unique( do.call(c, tlist ))
-xx <- lapply( tlist , function(x) which(x%in%tall) )
-names(xx) = samples[range]
-venn.diagram(xx, filename =paste0("vennDiagram_",0,".png"), height = 1800, width = 1800, fill=1:5 , alpha=0.3 ,imagetype = "png")
+vennDiagramGff(gffs[c(1:5)] , filename=paste0("vennDiagram_",0,".png") )
 
 for(i in 1:5)
 {
 	range = c(i,6:9)
-	tlist = lapply( gffs[range] , function(y) lapply( y , function(x) paste(paste(x[,1],x[,2])))   )
-	tall = unique( do.call(c, tlist ))
-	xx <- lapply( tlist , function(x) which(x%in%tall) )
-	names(xx) = samples[range]
-	venn.diagram(xx, filename =paste0("vennDiagram_",samples[i],".png"), height = 1800, width = 1800, fill=1:5 , alpha=0.3 ,imagetype = "png")
+	vennDiagramGff(gffs[range] , filename =paste0("vennDiagram_",samples[i],".png") )
+	
 }
+
+vennDiagramGff(mergeGffs[1:3] , filename ="vennDiagram_hiPSC_humanbrain.png" )
+vennDiagramGff(mergeGffs[2:4] , filename ="vennDiagram_patient_humanbrain.png" )
+
+
+
+
+
+
+
+
 
