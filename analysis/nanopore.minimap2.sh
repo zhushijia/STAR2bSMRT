df1aa1962f9e3b51e4eb8dcf4fdbcf6945e556bb
shijiazhu <shijiazhu@shijiazhus-MacBook-Pro.local>
2018-04-06 03:39:34 -0400

    nanopore try

---
+++ analysis/nanopore.minimap2.sh
@@ -0,0 +1,44 @@
+
+module load minimap2/r572
+ref="/hpc/users/zhus02/schzrnas/sjzhu/Project/NRXN/data/SIRV/reference/SIRV_Set1_Sequences_170612a/SIRV_isoforms_multi-fasta_170612a.fasta"
+seq="/hpc/users/zhus02/schzrnas/sjzhu/Project/NRXN/data/SIRV/E2_Nanopore/SRR5286959.fastq"
+cd /hpc/users/zhus02/schzrnas/sjzhu/Project/NRXN/data/SIRV/result/E2_Nanopore/SRR5286959_1
+minimap2 -ax splice --splice-flank=no $ref $seq > SRR5286959.sam
+minimap2 -x ava-ont $seq $seq > SRR5286959.paf
+
+
+
+module load star  
+cd /hpc/users/zhus02/schzrnas/sjzhu/Project/NRXN/data/SIRV/reference/SIRV_Set1_Sequences_170612a
+STAR --runMode genomeGenerate --genomeDir /hpc/users/zhus02/schzrnas/sjzhu/Project/NRXN/data/SIRV/reference/SIRV_Set1_Sequences_170612a/STAR_index --genomeFastaFiles SIRV_isoforms_multi-fasta_170612a.fasta  --runThreadN 10
+
+
+cd /hpc/users/zhus02/schzrnas/sjzhu/Project/NRXN/data/SIRV/result/E2_Nanopore/starLong/SRR5286959
+
+seq="/hpc/users/zhus02/schzrnas/sjzhu/Project/NRXN/data/SIRV/E2_Nanopore/SRR5286959.fastq"
+genomeDir="/hpc/users/zhus02/schzrnas/sjzhu/Project/NRXN/data/SIRV/reference/SIRV_Set1_Sequences_170612a/STAR_index"
+
+/hpc/users/zhus02/schzrnas/sjzhu/bitbucket/STAR/STAR/source/STARlong \
+--runMode alignReads \
+--readNameSeparator space \
+--outFilterMultimapScoreRange 1 \
+--outFilterMismatchNmax 2000 \
+--scoreGapNoncan -20 \
+--scoreGapGCAG -4 \
+--scoreGapATAC -8 \
+--scoreDelOpen -1 \
+--scoreDelBase -1 \
+--scoreInsOpen -1 \
+--scoreInsBase -1 \
+--alignEndsType Local \
+--seedSearchStartLmax 50 \
+--seedPerReadNmax 100000 \
+--seedPerWindowNmax 1000 \
+--alignTranscriptsPerReadNmax 100000 \
+--alignTranscriptsPerWindowNmax 10000 \
+--outReadsUnmapped Fastx \
+--outFilterIntronStrands None \
+--outSAMattributes NH HI AS nM NM MD jM jI \
+--genomeDir $genomeDir \
+--readFilesIn $seq
+
