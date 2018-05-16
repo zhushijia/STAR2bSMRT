1647aa16b2ee0b7141b64ffbf67afc6f9bcad17a
zhushijia <sjzhu.bioinfo@gmail.com>
2018-05-15 22:04:05 -0400

    mouse data

--- analysis/Mouse/mapping_starlong_34.R
+++ analysis/Mouse/mapping_starlong_34.R
@@ -4,18 +4,20 @@ library(foreach)
 library(doMC)
 
 cores = 30
-thresSR=c(1:100) 
+thresSR=c(1:500) 
 thresDis=c(1:30)
-adjustNCjunc=FALSE
+adjustNCjunc=TRUE  # TRUE and FALSE ts=408 td=15 num=281 frac=0.5069613
 fixedMatchedLS=FALSE
 chrom="chr17"
 s= 90036900
 e = 91089605
 registerDoMC(cores)
 
+annotation = read.table("/sc/orga/projects/schzrnas/sjzhu/Project/NRXN/data/Mouse_NRXN/Mouse_NRXN1_Annotation.txt",sep="\t",header=T)
+annotation[6,2] = 90704367
+annot_juncs = data.frame( js=annotation[2:nrow(annotation),2]+1 , je=annotation[1:(nrow(annotation)-1),1]-1 )
 
-outputDir="/hpc/users/zhus02/schzrnas/sjzhu/Project/NRXN/data/Mouse_NRXN/SRR1184043"
-
+outputDir="/hpc/users/zhus02/schzrnas/sjzhu/Project/NRXN/data/Mouse_NRXN/STAR2bSMRT"
 LoutputDir = paste0(outputDir,"/LR")
 SoutputDir = paste0(outputDir,"/SR")
 EoutputDir = paste0(outputDir,"/Exp")
@@ -25,11 +27,11 @@ system( paste0( "mkdir -p " , SoutputDir ) )
 system( paste0( "mkdir -p " , EoutputDir ) )
 
 SRalignment = paste0(SoutputDir,"/alignments.bam")
-LRalignment = paste0(LoutputDir,"/SRR1184043.sam")
+LRalignment = paste0(LoutputDir,"/Aligned.out.sam")
 
 
 SRjunc = getJunc( SRalignment , SoutputDir , chrom="chr17" , s= 90036900 , e = 91089605 )
-LRinfo = getLRinfo3( LRalignment , NULL , LoutputDir , chrom="chr17" , s= 90036900 , e = 91089605 , jI=FALSE)
+LRinfo = getLRinfo3( LRalignment , NULL , LoutputDir , chrom="chr17" , s= 90036900 , e = 91089605 , jI=TRUE)
 LRread = LRinfo$LRread
 LRjunc = LRinfo$LRjunc
 LRtag = LRinfo$LRtag
@@ -52,10 +54,16 @@ correction = generateCorrectedIsoform( LRjunc , SRjunc, LRtag , LRread  , ts , t
 correction[[1]]$frac
 correction[[1]]$num
 
+juncs = do.call(c,lapply(correction[[1]]$isoform[correction[[1]]$exp>2],function(x)paste(x[,2],x[,3],sep=",")))
+table(juncs)
+sort(unique(juncs))
+
+
+sites = c(sapply(strsplit(sort(unique(juncs)),","),function(x) as.integer(x)))
+
+
+#######################################################################################################
 setwd( EoutputDir )
-
-###############################################################################################################
-
 pdf( paste0( "JuncExp_LR_ts",ts,"_td",td,".pdf") )
 
 juncExp = do.call( rbind, lapply( correction , function(x) x$LSjuncCount ))
@@ -74,19 +82,20 @@ barplot( log10fc , cex.names=0.6 , col=cols , ylab="log10(lrCount/srCount)", nam
 
 dev.off()
 
+#######################################################################################################
+
+gffName = paste0( "isoform_ts",ts,"_td",td,".gff")
+exonList = juncToExon( juncList=correction[[chrom]]$isoform , s=90036900 , e=91089605 , exp=correction[[chrom]]$exp )
+writeGff( isoform=exonList , file = gffName )
 
 
+#######################################################################################################
+# check consistency with hiPSC
 
-t = t+1
-x = tag[[t]]
-y = x[ !x%in%correctTag ]
-tt = paste(LRjunc[[1]][,3],LRjunc[[1]][,4],sep=",")
-ind = SRmatch[tt%in%y , ]
-ind
-src[ind[,1],]
-src[ind[1],]
-y
+tag = lapply( exonList , function(x) apply(x,1,function(y) paste(y[2:3],collapse=",")) )
+annot_tag = apply(annotation,1,function(y) paste(y[1:2],collapse=",") )
 
+lapply(tag,function(x) as.character(annotation$Annotation)[ match(x,annot_tag) ]   )
 
 
 
