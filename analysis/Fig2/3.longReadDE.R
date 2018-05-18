3fce06710066fceaba2f283bc19e41b49099334b
zhushijia <sjzhu.bioinfo@gmail.com>
2018-05-18 18:40:52 -0400

    long read de

--- analysis/Fig2/3.longReadDE.R
+++ analysis/Fig2/3.longReadDE.R
@@ -20,26 +20,37 @@ contGff2 =  unionGff(gffs2[[1]],gffs2[[2]])
 caseGff2 =  unionGff(gffs2[[3]],gffs2[[4]])
 allGff2 =  unionGff(caseGff2,contGff2)
 
+length(allGff2)
+length(caseGff2) - sum(!is.na(matchGff(contGff2,caseGff2)))
+length(contGff2) - sum(!is.na(matchGff(caseGff2,contGff2)))
 
-allCoverage = c()
+
+
+
 expMatrix = sapply( 1:4 , function(i) {
   ee = exp2[[i]][ matchGff( allGff2 , gffs2[[i]] ) ]
   ee[is.na(ee)] = 0
   ee
 } )
-expMatrix = expMatrix/allCoverage
-colnames(expMatrix) = names(gffs)[1:4]
+
+allCoverage = c(ct2607=14.7, ct553=7.5, sz581=13.2, sz641=15.6)
+expMatrix2 = expMatrix/allCoverage
+colnames(expMatrix2) = names(gffs)[1:4]
 
 
 
-shared = apply( expMatrix, 1, function(x) all(x>0) )
+shared = apply( expMatrix2, 1, function(x) all(x>0) )
 
-de = t( apply( expMatrix , 1 , function(x) {
+de = t( apply( expMatrix2[shared,] , 1 , function(x) {
   cont = mean(x[1:2])
   case = mean(x[3:4])
   fc = case/cont
   p = t.test( x[1:2] , x[3:4] )$p.val
   c( case=case , cont=cont , fc=fc , p=p  )
 } ) )
+
+sum(de[,3]>2)
+sum(de[,3]<1/2)
+
 
 
